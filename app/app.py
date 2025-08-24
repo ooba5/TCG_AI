d433d72eea69a0cac376c5ce1ee32b7cfd69e1fa
ooba5 <cwheffer@gmail.com>
2025-08-24 13:53:45 -0400

    jumping to agentic rag

--- app/app.py
+++ app/app.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
 from typing import List, Dict
-from chat_functions import generate, clean_for_html
+from chat_functions import generate, clean_for_html, system_instructions_teach_rules, system_instructions_choose_game
 from fastapi.middleware.cors import CORSMiddleware
+from langchain_core.output_parsers import JsonOutputParser
 app = FastAPI()
 
 app.add_middleware(
@@ -12,6 +13,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+#Object for parsing JSON output from the agent
+parser = JsonOutputParser()
+
 # Global state for conversation history (NOT USER SPECIFIC)
 past_conversation: List[Dict[str, str]] = []
 
@@ -22,12 +26,25 @@ class QuestionRequest(BaseModel):
 @app.post("/ask")
 async def ask_question(request: QuestionRequest):
     # Each data store should represent the rulebook of one game trained into a RAG engine.
-    if request.game == "Default":
+    # The data store needs to be selected by an Agent based on the user's context.
+    agents_chosen_datastore = generate(request.query, system_instructions_choose_game, past_conversation, selected_datastore = None )
+    selected_datastore = parser.parse(agents_chosen_datastore.text).get('game', 'fallback')
+    # Retrieve the Correct Datastore
+    print(f"Selected Datastore: {selected_datastore}")
+    if selected_datastore.upper() == "RIFTBOUND":
+        selected_datastore = "projects/kinetic-calling-463721-a8/locations/us-central1/ragCorpora/3379951520341557248"
+    elif selected_datastore.upper() == "YUGIOH":
+        selected_datastore = "projects/kinetic-calling-463721-a8/locations/us-central1/ragCorpora/7454583283205013504"
+    elif selected_datastore.upper() == "POKEMON":
         selected_datastore = "projects/kinetic-calling-463721-a8/locations/us-central1/ragCorpora/4749045807062188032"
-    elif request.game == "Alt":
-        selected_datastore = "projects/812596078800/locations/us-central1/ragCorpora/7454583283205013504"
-    response = generate(request.query, past_conversation, selected_datastore=selected_datastore)
-    past_conversation.append({"role": "user", "content": request.query})
-    past_conversation.append({"role": "assistant", "content": response.text})
-    cleaned_text = clean_for_html(response.text)
+    else:
+        selected_datastore = "fallback"
+    # Simple fallback if the agent fails to choose a game (could use another agent here or a use a retry mechanism)
+    if selected_datastore == "fallback":  
+        return {"answer": "I'm sorry, I couldn't determine which game you're referring to. Please specify either Pokemon, Riftbound, or YuGiOh."}
+    else:  
+        response = generate(request.query, system_instructions_teach_rules, past_conversation, selected_datastore=selected_datastore)
+        past_conversation.append({"role": "user", "content": request.query})
+        past_conversation.append({"role": "assistant", "content": response.text})
+        cleaned_text = clean_for_html(response.text)
     return {"answer": cleaned_text}
--- app/chat_functions.py
+++ app/chat_functions.py
@@ -4,85 +4,100 @@ from google.genai import types
 from html import escape
 import markdown
 
-def generate(query, history = None, selected_datastore = "projects/812596078800/locations/us-central1/ragCorpora/7454583283205013504", 
-             selected_model = "gemini-2.5-flash-lite"):
-    """Generates content using Google Generative AI with specified datastore and model."""
-    client = genai.Client(
-      vertexai=True,
-      project="kinetic-calling-463721-a8",
-      location="global",
+def generate(query, system_instructions,
+            history = None, selected_datastore = None, 
+            selected_model = "gemini-2.5-flash-lite"):
+   """Generates content using Google Generative AI with specified datastore and model."""
+   client = genai.Client(
+   vertexai=True,
+   project="kinetic-calling-463721-a8",
+   location="global",
    )
-
-    si_text1 = """Role: You are an AI assistant dedicated to helping users learn and play a card games
-   Core Objective: To guide users through the game's rules, mechanics, and strategies in a clear, concise, and helpful manner, fostering a positive learning experience."""
-
-
-    tools = [
-        types.Tool(
-        retrieval=types.Retrieval(
-        vertex_rag_store=types.VertexRagStore(
+   if selected_datastore:
+      tools = [
+         types.Tool(
+         retrieval=types.Retrieval(
+         vertex_rag_store=types.VertexRagStore(
             rag_resources=[
             types.VertexRagStoreRagResource(
-                rag_corpus=selected_datastore
+                  rag_corpus=selected_datastore
             )
             ],
-        )
-        )
-    )
-   ]
-
-    generate_content_config = types.GenerateContentConfig(
-      temperature = 1,
-      top_p = 0.95,
-      max_output_tokens = 19629,
-      safety_settings = [types.SafetySetting(
-         category="HARM_CATEGORY_HATE_SPEECH",
-         threshold="OFF"
-      ),types.SafetySetting(
-         category="HARM_CATEGORY_DANGEROUS_CONTENT",
-         threshold="OFF"
-      ),types.SafetySetting(
-         category="HARM_CATEGORY_SEXUALLY_EXPLICIT",
-         threshold="OFF"
-      ),types.SafetySetting(
-         category="HARM_CATEGORY_HARASSMENT",
-         threshold="OFF"
-      )],
-      tools = tools,
-      system_instruction=[types.Part.from_text(text=si_text1)],
-      thinking_config=types.ThinkingConfig(
-         thinking_budget=0,
-      ),
-    )
-  
-    contents_list = []
-    if history:
-       for msg in history:
-          contents_list.append(
-            types.Content(
-               role=msg['role'],
-               parts=[types.Part.from_text(text=msg['content'])]
-            )
          )
+         )
+         )
+      ]
+   else: 
+      tools = []
+
+   generate_content_config = types.GenerateContentConfig(
+   temperature = 1,
+   top_p = 0.95,
+   max_output_tokens = 19629,
+   safety_settings = [types.SafetySetting(
+      category="HARM_CATEGORY_HATE_SPEECH",
+      threshold="OFF"
+   ),types.SafetySetting(
+      category="HARM_CATEGORY_DANGEROUS_CONTENT",
+      threshold="OFF"
+   ),types.SafetySetting(
+      category="HARM_CATEGORY_SEXUALLY_EXPLICIT",
+      threshold="OFF"
+   ),types.SafetySetting(
+      category="HARM_CATEGORY_HARASSMENT",
+      threshold="OFF"
+   )],
+   tools = tools,
+   system_instruction=[types.Part.from_text(text=system_instructions)],
+   thinking_config=types.ThinkingConfig(
+      thinking_budget=0,
+   ),
+   )
+  
+   contents_list = []
+   if history:
+      for msg in history:
+         contents_list.append(
+         types.Content(
+            role=msg['role'],
+            parts=[types.Part.from_text(text=msg['content'])]
+         )
+      )
 
 
-    contents_list.append(
-        types.Content(
-        role="user",
-        parts=[types.Part.from_text(text=query)]
-        )
-    )
+   contents_list.append(
+      types.Content(
+      role="user",
+      parts=[types.Part.from_text(text=query)]
+      )
+   )
    
-    response = client.models.generate_content(
-        model=selected_model,
-        contents=contents_list,
-        config=generate_content_config)
-    print(response.text)
-    
-    return response
+   response = client.models.generate_content(
+      model=selected_model,
+      contents=contents_list,
+      config=generate_content_config)
+   print(response.text)
+   
+   return response
 
 def clean_for_html(text):
 
     # Convert markdown to HTML
     html = markdown.markdown(text)
     return html
+
+system_instructions_choose_game = """
+You are an AI agent that analyzes user queries and conversation history to determine which of the following three games the user is referring to: Pokemon, Riftbound, or YuGiOh.
+Or choose "fallback" if you are not 100% sure, it should be clear as the user is will provide the game name in their query.
+Instructions:
+- Consider both the current user input and the historical conversation context.
+- Choose only one game from the list: Pokemon, Riftbound, YuGiOh. 
+- If one of those games has not been explicity written by the user, choose "fallback".
+- If you are unsure, do not guess; instead, respond with "fallback".
+- Output your answer strictly in the following machine-readable JSON format: {"game":"<game_name>"}
+- Replace <game_name> with either "pokemon", "riftbound", "yugioh", or "fallback (all UPPERCASE).
+- Do not include any explanation, extra text, or formatting—only output the JSON object.
+"""
+
+system_instructions_teach_rules = """Role: You are an AI assistant dedicated to helping users learn and play a card games Core 
+Objective: To guide users through the game's rules, mechanics, and strategies in a clear, concise, and helpful manner, fostering a positive learning experience."""
